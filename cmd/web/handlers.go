0184d6b2e660e7b79f151e94d79af48b10be0c1f
Mosope <104572982+sopesamuel@users.noreply.github.com>
2026-03-05 13:51:17 +0100

    user login details

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -170,9 +170,66 @@ func (app *application) userLogin(w http.ResponseWriter, r *http.Request){
 
 	app.render(w, r, http.StatusUnprocessableEntity, "login.tmpl", data)
 }
+
+
 func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintln(w, "Authenticate and login the user...")
+	
+	var form userLoginForm
+
+	err := app.decodePostForm(r, &form)
+	if err != nil{
+		app.clientError(w, r, http.StatusBadRequest)
+		return
+	}
+
+	form.CheckField(validator.NotBlank(form.Email), "Email", "Field cannot be blank!")
+	form.CheckField(validator.NotBlank(form.Password), "Password", "Field cannot be blank!")
+	form.CheckField(validator.Matches(form.Email, validator.EmailRX), "Email", "This field must be a valid email address!")
+	form.CheckField(validator.MinChars(form.Password, 8), "Password", "Password must be 8 characters long!")
+
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, r, http.StatusUnprocessableEntity, "login.tmpl", data)
+		return
+	}
+
+	id, err := app.users.Authenticate(form.Email, form.Password)
+	if err != nil {
+		if errors.Is(err, models.ErrInvalidCredentials){
+			form.AddNonField("Email or Password is incorrect!")
+			data := app.newTemplateData(r)
+			data.Form = form
+			app.render(w, r, http.StatusUnprocessableEntity, "login.tmpl", data)
+			return
+		} else {
+			app.serverError(w, r, err)
+		}
+	}
+
+	err = app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	app.sessionManager.Put(r.Context(), "authenticatedUserID", id)
+
+	http.Redirect(w, r, "/snippet/create", http.StatusSeeOther)
+
 }
+
+
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintln(w, "Logout the user...")
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+
+	app.sessionManager.Put(r.Context(), "flash", "You've been logged out successfully!")
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
