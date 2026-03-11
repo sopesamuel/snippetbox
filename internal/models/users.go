ec9bf317671f23d4c963dae550b17f011587db58
Mosope <104572982+sopesamuel@users.noreply.github.com>
2026-03-11 11:24:12 +0100

    Embedded files

--- internal/models/users.go
+++ internal/models/users.go
@@ -72,6 +72,11 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 	return id, nil
 }
 
-func (m *UserModel) Exists(id string) (bool, error) {
-	return false, nil
+func (m *UserModel) Exists(id int) (bool, error) {
+	var exists bool
+
+	stmt := `SELECT EXISTS(SELECT true FROM users WHERE id = ?)`
+
+	err := m.DB.QueryRow(stmt, id).Scan(&exists)
+	return exists, err
 }
