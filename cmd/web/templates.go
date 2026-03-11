f4d29b5b7c9ff4acf4e7f8a67b9dfc184855c335
Mosope <104572982+sopesamuel@users.noreply.github.com>
2026-03-11 21:50:21 +0100

    Embedding HTML Templates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"html/template"
+	"io/fs"
 	"path/filepath"
 	"time"
+
 	"snippetbox.project.sope/internal/models"
+	"snippetbox.project.sope/ui"
 )
 
 type templateData struct {
@@ -29,9 +32,7 @@ var functions = template.FuncMap{
 func newTemplateCache()(map[string]*template.Template, error){
 	cache := map[string]*template.Template{}
 
-	//helps to range/combine every file in pages with *
-	
-	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	pages, err := fs.Glob(ui.Files,"html/pages/*.tmpl")
 	if err != nil {
 		return nil, err
 	}
@@ -40,17 +41,13 @@ func newTemplateCache()(map[string]*template.Template, error){
 
 		name := filepath.Base(page)
 
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
-		if err != nil{
-			return nil, err
+		patterns := []string{
+			"html/partials/*.tmpl",
+			"html/base.tmpl",
+			page,
 		}
 
-		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
-		if err != nil{
-			return nil, err
-		}
-
-		ts, err = ts.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil{
 			return nil, err
 		}
