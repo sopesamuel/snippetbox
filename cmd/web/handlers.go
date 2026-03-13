32411a59a5151d25fdaa430ac62d58e4cad3b247
Mosope <104572982+sopesamuel@users.noreply.github.com>
2026-03-13 12:52:09 +0100

    Testing handlers and middlewares

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -30,7 +30,9 @@ type userLoginForm struct {
 	validator.Validator `form:"-"`
 }
 
-
+func ping(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("OK"))
+}
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	snippets, err := app.snippets.Latest()
